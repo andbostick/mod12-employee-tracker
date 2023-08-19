0f9f7047a27eac85f0b3406a8597a4c7907d3e0a
Andrew <andbostick@gmail.com>
2023-08-19 14:40:58 -0400

    started inquierer prompts

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-const mysql = require('mysql2')
-const inquirer = require('requirer')
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "employees_db",
+  },
+  console.log("connected to empoyee database")
+);
+
+const questions = [
+  {
+    type: "list",
+    name: "intro",
+    message: "What would you like to do?",
+    choices: [
+      "View All Departments",
+      "View All Roles",
+      "View All Employess",
+      "Add A Department",
+      "Add A Role",
+      "Add An Employee",
+      "Update An Employee Role",
+    ],
+  },
+];
+
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        if(answers.intro === 'View All Departments'){
+            db.query((`SELECT * FROM department ORDER BY id`), function (err,results){
+                console.table(results)
+            })
+        }
+    })
+}
+
+init()
