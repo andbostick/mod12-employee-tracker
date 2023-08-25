54905a5082b0c399ae95c45d39469eff6245b003
Andrew <andbostick@gmail.com>
2023-08-24 21:33:14 -0400

    created the ad employee

--- index.js
+++ index.js
@@ -29,23 +29,59 @@ const questions = [
 ];
 
 function init() {
+  //queries to create an array for later questions
+  let deptList = [];
+  let roleList = [];
+  let employeeList = [];
+
+  db.query(`SELECT * FROM department`, function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+    deptList = results.map((result) => result.name);
+  });
+
+  db.query(`SELECT * FROM role`, function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+    roleList = results.map((result) => result.title);
+  });
+
+  db.query(`SELECT * FROM employee`, function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+    employeeList = results.map((result) => result.first_name);
+    console.log(employeeList)
+  });
+
   inquirer.prompt(questions).then((answers) => {
+    //If User Selects view all departments option
     if (answers.intro === "View All Departments") {
       db.query(`SELECT * FROM department ORDER BY id`, function (err, results) {
-        console.table(results);
+        console.log(results);
+        init();
       });
     }
+
+    //if user Selects View all roles option
     if (answers.intro === "View All Roles") {
       db.query(`SELECT * FROM role ORDER BY id`, function (err, results) {
         console.table(results);
+        init();
       });
     }
+    //if user selects View all employees option
     if (answers.intro === "View All Employees") {
       db.query(`SELECT * FROM employee ORDER BY id`, function (err, results) {
         console.table(results);
+        init();
       });
     }
+    //if user selects add a department
     if (answers.intro === "Add A Department") {
+      //creates new inquirer questions
       inquirer
         .prompt([
           {
@@ -55,6 +91,7 @@ function init() {
           },
         ])
         .then((answers) => {
+          //makes a db call and adds a new department to the db
           db.query(
             `INSERT INTO department (name) VALUES (?)`,
             answers.name,
@@ -81,12 +118,14 @@ function init() {
             name: "salary",
           },
           {
-            type: "input",
+            type: "list",
             message: "Department For Role",
             name: "dept",
+            choices: deptList,
           },
         ])
         .then((answers) => {
+          //creates a promise to get the id for a department
           db.promise()
             .query(
               `SELECT id FROM department WHERE name=?`,
@@ -99,27 +138,90 @@ function init() {
               }
             )
             .then((result) => {
-              console.log(result[0][0].id);
+              //uses id from the promise to create the role
               db.query(
                 `INSERT INTO role (title,salary,department_id) VALUES (?, ?,?)`,
-                [answers.title,
-                answers.salary,
-                result[0][0].id],
+                [answers.title, answers.salary, result[0][0].id],
                 function (err, results) {
                   if (err) {
                     console.log(err);
                   }
                   console.log(`Succesfully added ${answers.title}`);
+                  init();
                 }
               );
             });
         });
     }
     if (answers.intro === "Add An Employee") {
-      console.log("added");
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What is the employees first name?",
+            name: "first_name",
+          },
+          {
+            type: "input",
+            message: "What is the employees last name?",
+            name: "last_name",
+          },
+          {
+            type: "list",
+            message: "What is the employees role?",
+            name: "role",
+            choices: roleList,
+          },
+        ])
+        .then((answers) => {
+          //creates a promise to get an id from the role
+          db.promise()
+            .query(
+              `SELECT id FROM role where title=?`,
+              answers.role,
+              function (err, results) {
+                if (err) {
+                  console.log(err);
+                }
+                return results;
+              }
+            )
+            .then((result) => {
+              //use that promise id to create a new employee
+              db.query(
+                `INSERT INTO employee(first_name, last_name, role_id, manager_id) values(?, ?, ?, ?)`,
+                [answers.first_name, answers.last_name, result[0][0].id, null],
+                function (err, results) {
+                  if (err) {
+                    console.log(err);
+                  }
+                  console.log(
+                    `added ${answers.first_name} ${answers.last_name}`
+                  );
+                  init();
+                }
+              );
+            });
+        });
     }
     if (answers.intro === "Update An Employee") {
-      console.log("updated");
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            message: "What is the employees role do you want to update",
+            name: "first_name",
+            choices: employeeList
+          },
+          {
+            type: "list",
+            message: "What is their new role?",
+            name: "last_name",
+            choices: roleList
+          },
+         
+          
+        ])
     }
   });
 }
